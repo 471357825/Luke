0e6b8bf123fcddb90c87cc9eb89332fd8fd8fc13
471357825 <75158154+471357825@users.noreply.github.com>
2021-02-05 01:11:52 +0800

    Update tanhua.js

--- tanhua.js
+++ tanhua.js
@@ -3,7 +3,7 @@ QuanX:
 
 [rewrite_local]
 #探花直播 by:Luke
-^https:\/\/1008610010\.laikanshu\.top\/Member\/getUserInfo url script-response-body tanhua.js
+^https:\/\/1008610010\.laikanshu\.top\/Member\/getUserInfo url script-response-body https://raw.githubusercontent.com/471357825/Luke/main/tanhua.js
 
 [mitm]
 1008610010.laikanshu.top
