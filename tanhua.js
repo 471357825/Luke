ebde58ced3d1798263bfc590cbded4cb271fb724
471357825 <75158154+471357825@users.noreply.github.com>
2021-02-11 02:44:39 +0800

    Update tanhua.js

--- tanhua.js
+++ tanhua.js
@@ -1,6 +1,8 @@
 /*
 QuanX:       
 公众号:捷径指令大全
+免登录，修复看直播退出登录
+下载地址:tanhua.app
 
 [rewrite_local]
 #探花直播 by:Luke
@@ -12,8 +14,20 @@ QuanX:
 */
 
 
-let obj = JSON.parse($response.body);
-obj.data.user_viptime = "2090-02-05",
-obj.data.is_vip = 1,
- 
+var obj = JSON.parse($response.body);
+obj = {
+  "status": "200",
+  "msg": "返回成功",
+  "data": {
+    "member_name": "捷径指令大全",
+    "mobile": "13588888888",
+    "head_pic": "http:\/\/999.junc.vip\/uploads\/admin\/202010\/5f9b91e0a6fe0.jpg",
+    "parent_id": 738,
+    "user_viptime": "2090-02-05",
+    "status": 1,
+    "is_vip": 1
+  }
+};
+
 $done({body: JSON.stringify(obj)});
+
