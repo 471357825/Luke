a0bf12439178cb9d16f8e8e95900e8a9bd5fa64b
471357825 <75158154+471357825@users.noreply.github.com>
2021-04-17 20:10:49 +0800

    Update madou.js

--- madou.js
+++ madou.js
@@ -9,9 +9,9 @@ https://mdsv01.com/?pc=157a3f6f-1440-4d8f-bf83-112cfc3e0fb1
 公众号：捷径指令大全
 
 QuanX:
-#麻豆短视频 by:Luke
 
 [rewrite_local]
+#麻豆短视频 by:Luke
 ^https:\/\/mda\.+\.site\/api\/(auth\/login|mine\/myHome) url script-response-body https://raw.githubusercontent.com/471357825/Luke/main/madou.js
 
 [MitM]
