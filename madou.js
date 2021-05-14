388cfccfb5b60e21bf888463f95483e2bcd51c0e
471357825 <75158154+471357825@users.noreply.github.com>
2021-05-14 15:40:30 +0800

    Update madou.js

--- madou.js
+++ madou.js
@@ -13,6 +13,8 @@ QuanX:
 [rewrite_local]
 #麻豆短视频 by:Luke
 ^https:\/\/mda\..+\.site\/api\/(auth\/login|mine\/myHome) url script-response-body https://raw.githubusercontent.com/471357825/Luke/main/madou.js
+^https:\/\/mda\..+\.site\/api\/operation\/video\/buy url response-body "code":\d+ response-body "code":200
+^https:\/\/mda\..+\.site\/api\/auth\/startupAdv url reject-200
 
 [MitM]
 mda.*.site
@@ -24,5 +26,9 @@ let obj = JSON.parse($response.body);
 obj.msg.user.vipLevel = "shallowCard";
 obj.msg.user.vipExpire = 9876543210000;
 obj.msg.user.name = "捷径指令大全";
+obj.msg.user.textMsgNum = 999;
+obj.msg.user.voiceMsgNum = 999;
+obj.msg.user.voteNum = 999;
+obj.msg.balance = 999;
 
 $done({body: JSON.stringify(obj)});
