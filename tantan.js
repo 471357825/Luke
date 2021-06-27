8d1392bd52ac6a5be4a134fe4abaccca480313d3
471357825 <75158154+471357825@users.noreply.github.com>
2021-06-27 23:46:25 +0800

    Update tantan.js

--- tantan.js
+++ tantan.js
@@ -5,15 +5,12 @@
 #探探网页版 by:Luke
 #观看地址 http://fnidf.buzz?uid=dvraowx4 
 #如打不开请用这个地址 http://iykhb.xyz/My?userId=1406787851020423168
-
-^http:\/\/iykhb\.xyz\/tantan\/mobile\/tantan\/user\/login url script-response-body https://raw.githubusercontent.com/471357825/Luke/main/tantan.js
-^http:\/\/iykhb\.xyz\/tantan\/mobile\/tantan\/user\/addNm url response-body "code":\d+ response-body "code":1
-
+^http:\/\/(.+)\.(+)\/tantan\/mobile\/tantan\/user\/login url script-response-body https://raw.githubusercontent.com/471357825/Luke/main/tantan.js
+^http:\/\/(.+)\.(+)\/tantan\/mobile\/tantan\/user\/addNm url response-body "code":\d+ response-body "code":1
 [mitm]
-hostname= iykhb.xyz
+*.*
 
 */
-
 
 
 var obj = JSON.parse($response.body);
