873cafdaff36106c4a4d528c8234aaac3180ddb7
471357825 <75158154+471357825@users.noreply.github.com>
2021-06-27 22:13:29 +0800

    Create tantan.js

---
+++ tantan.js
@@ -0,0 +1,22 @@
+/*
+公众号：捷径指令大全
+
+[rewrite_local]
+#探探网页版 by:Luke
+^http:\/\/iykhb\.xyz:8080\/tantan\/mobile\/tantan\/user\/login url script-response-body https://raw.githubusercontent.com/471357825/Luke/main/tantan.js
+^http:\/\/iykhb\.xyz:8080\/tantan\/mobile\/tantan\/user\/addNm url response-body "code":\d+ response-body "code":1
+
+[mitm]
+hostname= iykhb.xyz
+
+*/
+
+
+
+var obj = JSON.parse($response.body);
+    obj.data.frequency = 99999,
+    obj.data.name = "捷径指令大全",
+    obj.data.isVip = "1",
+    obj.data.maturityDate=4000000000000,
+    obj.data.maturityDates = "2096-10-02 15:06:40",
+$done({body: JSON.stringify(obj)});
