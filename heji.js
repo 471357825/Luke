36432053a20097e490d436b973694f0a4ca22cea
471357825 <75158154+471357825@users.noreply.github.com>
2021-07-05 19:14:19 +0800

    Update heji.js

--- heji.js
+++ heji.js
@@ -1,3 +1,10 @@
+[rewrite_local]
+#2496音乐 公众号：捷径指令大全  by：Luke
+^http:\/\/api_2496\.kuwo\.cn\/front\/user\/vipstatus url script-response-body https://raw.githubusercontent.com/471357825/Luke/main/2496.js
+
+[mitm]
+api_2496.kuwo.cn
+
 
 [rewrite_local]
 #杏仁直播 by:Luke
