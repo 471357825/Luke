55edc1c78a01a1b371b447baaf497fd4d7a5df25
471357825 <75158154+471357825@users.noreply.github.com>
2021-07-05 19:40:12 +0800

    Update ruxue.js

--- ruxue.js
+++ ruxue.js
@@ -4,7 +4,7 @@
 [rewrite_local]
 #如雪直播  下载地址：
 #公众号：捷径指令大全 byLuke
-^https:\/\/xw-lv\.hengmanjiankang\.cn\/api\/publi\/(.+)
+^https:\/\/xw-lv\.hengmanjiankang\.cn\/api\/publi\/(.+) url script-response-body https://raw.githubusercontent.com/471357825/Luke/main/ruxue.js
 
 [MitM]
 xw-lv.hengmanjiankang.cn
