2267b8177eb987e38b9ac6ac3a7252af8e792c6b
471357825 <75158154+471357825@users.noreply.github.com>
2021-07-10 10:13:00 +0800

    Create jiateng.js

---
+++ jiateng.js
@@ -0,0 +1,19 @@
+/*
+[rewrite_local]
+#加藤视频  公众号：捷径指令大全  by:Luke
+^https:\/\/api\.(.+)\.cc\/shorter\/(video\/hot\/list|home\/list|user\/getUser|user\/homePage|user\/homePageExt|resource\/getWindowNotice) url script-response-body https://raw.githubusercontent.com/471357825/Luke/main/jiateng.js
+下载地址:
+https://www.sanminnongye.com/?invitationCode=YMG82M
+[MITM]
+api.*.cc
+*/
+
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+body=body.replace(/userVip":\d/g,'userVip":1').replace(/expiredTime":\d+/g,'expiredTime":9876543210000').replace(/isVip":\d/g,'isVip":0').replace(/nickname":.+?,/g,'vip_end_date":"捷径指令大全",').replace(/isCharge":\d/g,'isCharge":0').
+
+
+$done({body});
