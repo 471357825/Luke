2272949558fa3e0a0ab98dab4a1e98985105bf54
471357825 <75158154+471357825@users.noreply.github.com>
2021-07-10 11:05:46 +0800

    Update jiateng.js

--- jiateng.js
+++ jiateng.js
@@ -13,6 +13,6 @@ var body = $response.body;
 var url = $request.url;
 var obj = JSON.parse(body);
 
-body=body.replace(/userVip":\d/g,'userVip":1').replace(/expiredTime":\d+/g,'expiredTime":9876543210000').replace(/isVip":\d/g,'isVip":0').replace(/nickname":.+?,/g,'nickname":"捷径指令大全",').replace(/isCharge":\d/g,'isCharge":0').replace(/headImage":.+?,'headImage": "/group1/M00/03/98/rB8KgmDpC7mAX6RfAABez1rGnxE914.jpg",').replace(/content":.+?,/g,'content":"关注微信公众号：捷径指令大全",');
+body=body.replace(/userVip":\d/g,'userVip":1').replace(/expiredTime":\d+/g,'expiredTime":9876543210000').replace(/isVip":\d/g,'isVip":0').replace(/nickname":.+?,/g,'nickname":"捷径指令大全",').replace(/isCharge":\d/g,'isCharge":0').replace(/headImage":.+?,/g,'headImage":"/group1/M00/03/98/rB8KgmDpC7mAX6RfAABez1rGnxE914.jpg",').replace(/content":.+?,/g,'content":"关注微信公众号：捷径指令大全",');
 
 $done({body});
